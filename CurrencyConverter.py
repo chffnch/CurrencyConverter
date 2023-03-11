c759fe2509cb86c44e2cf8c216edafced1af2e76
Irina Lutz <irina@lutz.li>
2023-03-11 09:46:04 +0100

    Currency converter

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -1,15 +1,31 @@
+import requests
+import json
 
-#conicoins = float(input("Please, enter the number of conicoins you have: "))
-conicoins = float(input())
+ccy_from = input("Currency from: ").lower()
 
-rates = {'RUB': 2.98,
-         'ARS': 0.82,
-         'HNL': 0.17,
-         'AUD': 1.9622,
-         'MAD': 0.208}
+# Retrieve exchange rates for EUR and USD from the online source
+r = requests.get('http://www.floatrates.com/daily/eur.json')
 
-for currency, value in rates.items():
-    print(f"I will get {round(conicoins * value, 2)} {currency} from the sale of {conicoins} conicoins.")
+cache = dict()
+cache["eur"] = json.loads(requests.get('http://www.floatrates.com/daily/eur.json').text)
+cache["usd"] = json.loads(requests.get('http://www.floatrates.com/daily/usd.json').text)
 
-
+while True:
+    ccy_to = input("Currency to: ").lower()
+    
+    if ccy_to == "":
+        print("End of session.")
+        break
+    amt_ccy_from = float(input("Amount currency from: "))
+    print("Checking the cache...")
+    
+    try:
+        cache[ccy_to]
+        print("Oh! It is in the cache!")
+    except KeyError:
+        print("Sorry, but it is not in the cache!")
+        cache[ccy_to] = json.loads(requests.get(f'http://www.floatrates.com/daily/{ccy_to}.json').text)
+    
+    amt_ccy_to = round(amt_ccy_from * cache[ccy_to][ccy_from]['inverseRate'], 2)
+    print(f"You received {amt_ccy_to} {ccy_to.upper()}.")
 
---
+++ README.txt
@@ -0,0 +1,2 @@
+This is a currency converter.
+To finish the calculation the entry to "currency to" must be empty.
